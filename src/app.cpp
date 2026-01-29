79a16b3c84e6771a5f173c2a2909764f0fd96d36
Jonas Welle <jonashwelle@proton.me>
2026-01-29 15:09:02 +0100

    I think we're already figuring out a much better way to do things. The idea is, isolate the windows header to one translation unit, and the vulkan header to another translation unit, and make the main translation unit include neither. That way, as long as we're not modifying the renderer or windows code, we will get fast compile times.

---
+++ src/app.cpp
@@ -0,0 +1,17 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include "app.h"
+
+namespace app
+{
+	void init()
+	{
+
+	}
+
+	bool update()
+	{
+		return false;
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,14 @@
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include "app.h"
+#include "renderer.h"
 
-#include <print>
-#include <system_error>
-
-#include <vulkan/vulkan.h>
-
-struct WindowClass 
+int main() 
 {
-	WNDCLASSEXW desc;
+	app::init();
+
+	while (app::update()) 
+	{
+
+	}
 	
-	WindowClass(WNDCLASSEXW _desc) : desc(_desc) 
-	{
-		if (!RegisterClassExW(&desc)) 
-		{
-			throw std::system_error{0};
-		}
-	}
-
-	~WindowClass() 
-	{
-		if (!UnregisterClassW(desc.lpszClassName, desc.hInstance)) 
-		{
-
-		}
-	}
-};
-
-struct Window {
-	HWND handle;
-
-	Window(
-			DWORD     dwExStyle,
-  			LPCWSTR   lpClassName,
-  			LPCWSTR   lpWindowName,
-			DWORD     dwStyle,
-			int       X,
-			int       Y,
-			int       nWidth,
-			int       nHeight,
-  			HWND      hWndParent,
-  			HMENU     hMenu,
-  			HINSTANCE hInstance,
-  			LPVOID    lpParam) 
-	{
-		handle = CreateWindowExW(
-			dwExStyle,
-			lpClassName,
-			lpWindowName,
-			dwStyle,
-			X,
-			Y,
-			nWidth,
-			nHeight,
-			hWndParent,
-			hMenu,
-			hInstance,
-			lpParam);
-		if (!handle)
-		{
-
-		}
-	}
-
-	~Window()
-	{
-
-	}
-};
-
-int main() {
-	WNDCLASSEXW window_class_desc {
-
-	};
-	WindowClass window_class {window_class_desc};
-
-	std::println("Hello, world!");
 	return 0;
 }
