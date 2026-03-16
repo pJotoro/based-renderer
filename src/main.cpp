ff4b37dad9eecf904636a197d643284505802919
Jonas Welle <jonashwelle@proton.me>
2026-03-16 17:07:50 +0100

    Almost there!

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,4 @@
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-
-// Windows.h defines these macros, which screw with certain things in the C++ standard library.
-#ifdef max
-#undef max
-#endif
-#ifdef min
-#undef min
-#endif
-
-#include <slang/slang-com-ptr.h>
-
-#define VK_USE_PLATFORM_WIN32_KHR
-import vulkan;
+#include <pch.hpp>
 
 // TODO: Would it make sense to add BASED_RENDERER_ to these macro names?
 #define UNUSED(X) (void)(X)
