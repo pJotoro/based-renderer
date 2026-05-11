129a41aa91761f6246d621e122911b2e04823c4b
Jonas Welle <jonashwelle@proton.me>
2026-05-11 12:22:30 +0200

    Added cgltf.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#define CGLTF_IMPLEMENTATION
+#pragma warning(push)
+#pragma warning(disable: 4996)
+#include "cgltf.h"
+#pragma warning(pop)
+
 namespace based_renderer
 {
 
