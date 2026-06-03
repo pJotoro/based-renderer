d34a74a645194eca77c18a98297c2105275afaf7
Jonas Welle <jonashwelle@proton.me>
2026-06-03 13:50:24 +0200

    Switched back to using stb image as a package instead of just having it directly in the project.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+// TODO: Add Findcgltf.cmake and remove cgltf.h
 #define CGLTF_IMPLEMENTATION
 #pragma warning(push)
 #pragma warning(disable: 4996)
